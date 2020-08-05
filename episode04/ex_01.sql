dccc14d19a9251311181cfa33202b757eb4f36bd
Raniere Ramos <ramos.raniere@gmail.com>
2020-08-04 22:29:22 -0300

    Add files via upload

---
+++ episode04/ex_01.sql
@@ -0,0 +1,12 @@
+ -- Some exercises about concepts of episode 04
+ 
+ -- Ex.1: Qual o valor total de receita gerada por consumidores de cada estado?
+ -- Considere a base completa, com apenas pedidos entregues.
+
+ -- Ex.2: Qual o valor total de receita gerada por sellers de cada estado?
+ -- Considere a base completa, com apenas pedidos entregues.
+
+ -- Ex.3: Qual o peso médio dos produtos vendidos por sellers de cada estado?
+ -- Considere apenas o ano de 2017 e pedidos entregues nesta análise.
+
+ 
